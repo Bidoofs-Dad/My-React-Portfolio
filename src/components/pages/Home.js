1f9f1d89bd1b0131cfc8108a5fd3eaa76c8c8f81
Zane Werle <zanewerle@gmail.com>
2023-07-26 15:35:36 -0500

    added in some of the styling to the Home and Footer js files

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,18 @@
 import React from "react";
+import '../styles/Home.css';
 
 export default function Home() {
     return(
-    <div>
-        <h2>Home</h2>
+    <div className="home">
+        <h1>Home</h1>
+        <br></br>
+        <br></br>
+        <div>
         <p>
             Welcome to my Portfolio! Use the navigation bar above and feel free to have a look around!
         </p>
+        <p>If you have any questions, feel free to reach out to me via the Contact page!</p>
+        </div>
     </div>
     );
 };
