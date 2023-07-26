34f4c7e1563f2dbcc1167665dc7da88bf28b2458
Zane Werle <zanewerle@gmail.com>
2023-07-26 14:50:41 -0500

    added in the NavTabs.js and filled in the code for the PortfolioConotainer.js to handlw the page changes.

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import NavTabs from './NavTabs';
+import Home from './pages/Home';
+import AboutMe from './pages/AboutMe';
+import Portfolio from './pages/portfolio/Portfolio';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
+
+export default function PortfolioContainer() {
+    const [currentPage, setCurrentPage] = useState('Home');
+  
+    const renderPage = () => {
+      if (currentPage === 'Home') {
+        return <Home />;
+      }
+      if (currentPage === 'AboutMe') {
+        return <AboutMe />;
+      }
+      if (currentPage === 'Portfolio') {
+        return <Portfolio />;
+      }
+      if (currentPage === 'Contact') {
+        return <Contact />;
+      }
+      return <Resume />;
+    };
+  
+    const handlePageChange = (page) => setCurrentPage(page);
+  
+    return (
+      <div>
+        <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+        {renderPage()}
+      </div>
+    );
+  }
