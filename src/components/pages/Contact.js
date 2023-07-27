a13edc2836812938ba955ddf76d56813137bbb49
Zane Werle <zanewerle@gmail.com>
2023-07-26 19:13:52 -0500

    added in the code for the form on the contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,12 +1,18 @@
 import React from "react";
 
 export default function Contact() {
-    return(
-    <div>
-        <h2>Contact</h2>
-        <p>
-            Welcome to my Portfolio! Use the navigation bar above and feel free to have a look around!
-        </p>
-    </div>
+    return (
+        <div>
+            <h2>Contact</h2>
+            <form>
+                <label for="name">Name:</label><br></br>
+                <input type="text" id="name" name="name"></input><br></br>
+                <label for="email">Email:</label><br></br>
+                <input type="text" id="email" name="email"></input><br></br>
+                <label for="message">Message:</label><br></br>
+                <textarea id="message" name="message" rows="5" cols="20"></textarea><br></br>
+                <input type="submit" id="submit" value="Submit"></input>
+            </form>
+        </div>
     );
 };
