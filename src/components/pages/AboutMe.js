e49d1edaf368c41dcfb4f15cca7310e22d2447af
Zane Werle <zanewerle@gmail.com>
2023-07-27 16:43:29 -0500

    added in the About Me text

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -1,12 +1,55 @@
 import React from "react";
+import '../styles/AboutMe.css'
+import Me from '../../assets/Me.jpg'
 
 export default function AboutMe() {
-    return(
-    <div>
-        <h2>About Me!</h2>
-        <p>
-            Welcome to my Portfolio! Use the navigation bar above and feel free to have a look around!
-        </p>
-    </div>
+    return (
+        <div>
+            <h2>About Me!</h2>
+            <img src={Me} alt="Avatar"></img>
+            <p>
+                Hi there! Thank you first off for the interest in learning more About Me! My Name is Zane and boy howdy do I have a lot of interests. I absolutely love spending time with my wonderful wife, we enjoy long-boarding together, watching movies and just getting out into nature to travel and hike!
+                Some of my other interests are skateboarding, playing video games and doing anything that is related to art!
+                <br></br>
+                <br></br>
+                I started skateboarding at a young age and have been in and out of it for years. As of the summer of 2022, I got back into it heavily and have been learning a bunch of new things that just really gets the dopamine flowing! Unfortunately about 6 months out of the year, I can't really enjoy it due to the awful winters here (yes I live in Minnesota) I could in theory spend more time at in-door skateparks, but I like to try and avoid driving on ice as much as possible haha!
+                <br></br>
+                <br></br>
+                I have been into gaming since I was very young and it has just grown into a passion! I play almost any genre (aside from sports games really) but my absolute favorite genre of game (and movie at that) is horror! I love spooky related things so much that my wife and I decided that Halloween would be the perfect wedding day! (she loves horror too, this isn't a one sided love haha!)
+                <br></br>
+                <br></br>
+                My wife and I love cats, so much so that we have 4! Together though, we have had a total of 5 of our own! Their names are;
+
+                <ul>
+                    <li>Luna (RIP she was the sweetest and goofiest cat I think I've ever met)</li>
+                    <li>Ozzie Pawsbourne: Kitty Of Darkness</li>
+                    <li>Ted Bundy (trust me, its fitting, he is the sweetest boy, but he also thinks that claws need to be apart of "love")</li>
+                    <li>Cake The Cat</li>
+                    <li>Chucky</li>
+                </ul>
+
+                They Are all just the absolute cutest little goofballs! (I should add some photos of them in the future)
+                <br></br>
+                <br></br>
+                I am a firm believer in just being a nice person, I think that life is way too short to treat others poorly, so I make it my aim to just try and be as genuine of a human that I can be! With that I have made some incredible friends along the way that I would absolutely do anything for! Knowing kindness truly builds the strongest frienships in my opinion!
+                <br></br>
+                <br></br>
+                That being said, on a more 'Work' related side of things (that is why you are here I pressume 😏)<br></br>
+                I love learning new things! I believe that the more knowledge one can learn, the more one can explore different interests! So I always like to look out and learn new things (even if its just a goofy little movie triva) any new knowledge helps one grow!<br></br>
+                As for working, I have a strong work ethic and catch on quickly to new environments and job related tasks! Even if I am in a work situation that is not ideal, I still try and give it my all, just because I feel that refelects who I truly am! On top of that, I am definitely a people person, I never find it hard to make new friends and colleagues no matter where I am at. Whether it be a frienship or customer relationship, I just treat people like people and the rest speaks for itself! I feel like I have a lot to bring to the table, no matter what job I may be working, my eagerness to learn more and try to brighten one's day I think go hand in hand and are qualities I feel I reflect strongly!
+                <br></br>
+                <br></br>
+                That being said....
+                <br></br>
+                <br></br>
+                <span id='hireMe'>Hire Me!</span>
+                <br></br>
+                <br></br>
+                Sorry about that, I couldn't resist! Anyways, I think that is about enough about me, thank you again for taking the time to get to know me a little better! Feel free to checkout my portfolio and my resume! Then if you have any questions or inquiries, feel free to get ahold of me via the Contact Me tab!
+                <br></br>
+                <br></br>
+                I hope you have a wonderul rest of your day! 🖤
+                </p>
+        </div>
     );
 };
